1e008188cc066d3273fa501092516cbf452d0491
Abhay Kumar Chaudhary <akcofficial2005@gmail.com>
2024-07-11 00:59:40 +0530

    Add files via upload
    Dictionery and Sets

---
+++ Chapter 5/02_dict_methods.py
@@ -0,0 +1,14 @@
+marks ={
+    "Harry":100,
+    "Shubham":56,
+    "Rohan":23,
+    0: "Harry"
+}
+
+# print(marks.items())
+# print(marks.keys())
+# print(marks.values())
+# marks.update({"Harry": 99, "Renuka": 100})
+# print(marks)
+print(marks.get("Harry2")) # Prints None
+print(marks["Harry2"]) # Returns an Error
---
+++ Chapter 5/03_sets.py
@@ -0,0 +1,5 @@
+s = {1, 5, 32, 54, 5, 5, 5}
+
+e = set() # Don't use s = {} as it will create an empty dictionery
+
+print(s)
